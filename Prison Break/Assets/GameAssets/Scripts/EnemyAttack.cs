3e4adef3f4954779eddd3196d11343485a8afb36
unknown <rcandrew@millersville.edu>
2019-11-13 22:24:50 -0500

    Added Battle Scene

---
+++ Prison Break/Assets/GameAssets/Scripts/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public static int enemyNum;
+    public int x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyNum = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Punch.turn == 2 && enemyNum > 0)
+        {
+            for (int i = 0; i < enemyNum; i++)
+            {
+                x = Random.Range(1, 11);
+                if (x < 3)
+                {
+                    PlayerSanity.Sanity -= 1;
+
+                }
+            }
+            Punch.turn = 1;
+        }
+    }
+}
