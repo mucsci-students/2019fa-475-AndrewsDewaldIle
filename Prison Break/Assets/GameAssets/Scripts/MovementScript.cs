ca42df97e4c01dd0c1ec387d3bf43216820a533f
Dylan Dewald <dylan.dewald94@gmail.com>
2019-11-20 17:15:30 -0500

    Player made and moving with movementScript.

---
+++ Prison Break/Assets/GameAssets/Scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour{
+    private const float tileSize = 0.5f;
+    private Vector3 position;
+    public float positionTransferTime;
+    // Start is called before the first frame update
+    void Awake(){
+        position = new Vector3(0.0f,0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //Tile size of 8 by 8
+        //Size not sure yet.
+        if (Input.GetKeyDown("up")){
+            position.y += 1;
+        }
+        else if (Input.GetKeyDown("down")){
+            position.y -= 1;
+        }
+        else if (Input.GetKeyDown("left")){
+            position.x -= 1;
+        }
+        else if (Input.GetKeyDown("right")) {
+            position.x += 1;
+        }
+        transform.position = Vector3.Lerp(transform.position, position * tileSize, positionTransferTime);
+
+    }
+}
