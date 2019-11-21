bc70d2c70adb817c51b4edbc5fed20171d2c3a78
Dylan Dewald <dylan.dewald94@gmail.com>
2019-11-20 19:58:25 -0500

    Commented out some code and will work on layout of map next.

--- Prison Break/Assets/GameAssets/Scripts/OverWorldScripts/PlayerScript.cs
+++ Prison Break/Assets/GameAssets/Scripts/OverWorldScripts/PlayerScript.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour{
     private int sanityLevel;
-    private float lastTime;
+    //Testing purposes: private float lastTime;
     // Start is called before the first frame update
     void Awake()
     {
         sanityLevel = 0;
-        lastTime = Time.time;
+        //Testing purposes: lastTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time - lastTime >= 2.0f) {
-            gotHurt();
-            lastTime = Time.time;
-        }
+        //Testing purposes: 
+            //if (Time.time - lastTime >= 2.0f) {
+            //    gotHurt();
+            //     lastTime = Time.time;
+            //}
     }
 
     public int getSanityLevel() {
