53878ffdec77e5d7fd26ca76be361b126f25a57f
Gina M Ile <gmmarti1@lederberg.muad.local>
2019-12-05 12:54:46 -0500

    added all scenes to build, added title screen in unity

---
+++ Prison Break/Assets/GameAssets/Scripts/TitleScreen/TitleScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    /* TODO
+     * get button components
+     * make start button change scene to overworld scene
+     * make exit button close out of the game
+     * 
+    */
+
+    public GameObject startButton;
+    public GameObject exitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("OverWorld");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
